951b43a12b960e28fdf1c5820c0bb06c70fe2b66
xremix <tonidhoffmann@gmail.com>
2020-08-31 09:20:15 +0200

    clean up

--- projects/x-gallerify/src/lib/x-gallerify.component.ts
+++ projects/x-gallerify/src/lib/x-gallerify.component.ts
@@ -1,53 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+  import { Component } from '@angular/core';
 
-@Component({
-  selector: 'ui-xGallerify',
+  @Component({
+    selector: 'ui-xGallerify',
 
-  templateUrl: './x-gallerify.component.html',
-  styleUrls: ['./x-gallerify.component.scss']
-})
-export class XGallerifyComponent implements OnInit {
-  public images: Array<any> = [
-      {
-        src: "https://farm66.staticflickr.com/65535/49489692872_a202029a13_b.jpg",
-      },
-      {
-        src: "https://farm66.staticflickr.com/65535/49489489251_21a1775a60_b.jpg",
-      },
-      {
-        src: "https://farm66.staticflickr.com/65535/49489694752_f8d8745a85_b.jpg",
-      },
-      {
-        src: "https://farm66.staticflickr.com/65535/49488995108_5283113839_b.jpg",
-      },
-      {
-        src: "https://farm66.staticflickr.com/65535/49488995108_5283113839_b.jpg",
-      },
-      {
-        src: "https://farm66.staticflickr.com/65535/49488995108_5283113839_b.jpg",
-      },
-      {
-        src: "https://farm66.staticflickr.com/65535/49488995108_5283113839_b.jpg",
-      }
-    ];
-  constructor() { }
+    templateUrl: './x-gallerify.component.html',
+    styleUrls: ['./x-gallerify.component.scss']
+  })
+  export class XGallerifyComponent {
+    public images: Array<any> = [
+        {
+          src: "https://farm66.staticflickr.com/65535/49489692872_a202029a13_b.jpg",
+        },
+        {
+          src: "https://farm66.staticflickr.com/65535/49489489251_21a1775a60_b.jpg",
+        },
+        {
+          src: "https://farm66.staticflickr.com/65535/49489694752_f8d8745a85_b.jpg",
+        },
+        {
+          src: "https://farm66.staticflickr.com/65535/49488995108_5283113839_b.jpg",
+        },
+        {
+          src: "https://farm66.staticflickr.com/65535/49488995108_5283113839_b.jpg",
+        },
+        {
+          src: "https://farm66.staticflickr.com/65535/49489694752_f8d8745a85_b.jpg",
+        },
+        {
+          src: "https://farm66.staticflickr.com/65535/49488995108_5283113839_b.jpg",
+        },
+        {
+          src: "https://farm66.staticflickr.com/65535/49488995108_5283113839_b.jpg",
+        }
+      ];
 
-  ngOnInit(): void {
-  }
+    imageLoaded(event: any){
+      console.log(event);
+      var img = event.path[0];
+      var ratio  = img.width / img.height;
 
-
-  imageLoaded(event: any){
-    console.log(event)
-    var img = event.path[0];
-    var width = img.width;
-    var height = img.height;
-    var ratio  = width / height;
-
-    for (let i = 0; i < this.images.length; i++) {
-      if(this.images[i].src === img.src){
-        this.images[i].ratio = ratio;
+      for (let i = 0; i < this.images.length; i++) {
+        if(this.images[i].src === img.src){
+          this.images[i].ratio = ratio;
+        }
       }
     }
   }
-
-}
