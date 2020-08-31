aeaec69561cb981b8f9748b57ff6aa4976543f0d
xremix <tonidhoffmann@gmail.com>
2020-08-31 09:59:38 +0200

    chunk gallery into rows

--- projects/x-gallerify/src/lib/x-gallerify.component.ts
+++ projects/x-gallerify/src/lib/x-gallerify.component.ts
@@ -1,48 +1,63 @@
-  import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
-  @Component({
-    selector: 'ui-xGallerify',
+@Component({
+  selector: 'ui-xGallerify',
 
-    templateUrl: './x-gallerify.component.html',
-    styleUrls: ['./x-gallerify.component.scss']
-  })
-  export class XGallerifyComponent {
-    public images: Array<any> = [
-        {
-          src: "https://farm66.staticflickr.com/65535/49489692872_a202029a13_b.jpg",
-        },
-        {
-          src: "https://farm66.staticflickr.com/65535/49489489251_21a1775a60_b.jpg",
-        },
-        {
-          src: "https://farm66.staticflickr.com/65535/49489694752_f8d8745a85_b.jpg",
-        },
-        {
-          src: "https://farm66.staticflickr.com/65535/49488995108_5283113839_b.jpg",
-        },
-        {
-          src: "https://farm66.staticflickr.com/65535/49488995108_5283113839_b.jpg",
-        },
-        {
-          src: "https://farm66.staticflickr.com/65535/49489694752_f8d8745a85_b.jpg",
-        },
-        {
-          src: "https://farm66.staticflickr.com/65535/49488995108_5283113839_b.jpg",
-        },
-        {
-          src: "https://farm66.staticflickr.com/65535/49488995108_5283113839_b.jpg",
-        }
-      ];
-
-    imageLoaded(event: any){
-      console.log(event);
-      var img = event.path[0];
-      var ratio  = img.width / img.height;
-
-      for (let i = 0; i < this.images.length; i++) {
-        if(this.images[i].src === img.src){
-          this.images[i].ratio = ratio;
-        }
-      }
+  templateUrl: './x-gallerify.component.html',
+  styleUrls: ['./x-gallerify.component.scss']
+})
+export class XGallerifyComponent implements OnInit {
+  public maxPerRow: number = 4;
+  public images: Array<any> = [
+    {
+      src: "https://farm66.staticflickr.com/65535/49489692872_a202029a13_b.jpg",
+    },
+    {
+      src: "https://farm66.staticflickr.com/65535/49489489251_21a1775a60_b.jpg",
+    },
+    {
+      src: "https://farm66.staticflickr.com/65535/49489694752_f8d8745a85_b.jpg",
+    },
+    {
+      src: "https://farm66.staticflickr.com/65535/49488995108_5283113839_b.jpg",
+    },
+    {
+      src: "https://farm66.staticflickr.com/65535/49488995108_5283113839_b.jpg",
+    },
+    {
+      src: "https://farm66.staticflickr.com/65535/49489694752_f8d8745a85_b.jpg",
+    },
+    {
+      src: "https://farm66.staticflickr.com/65535/49488995108_5283113839_b.jpg",
+    },
+    {
+      src: "https://farm66.staticflickr.com/65535/49488995108_5283113839_b.jpg",
     }
+  ];
+  public rows: Array<Array<any>> = [];
+
+  ngOnInit(): void {
+    this.rows = this.chunkArray(this.images, this.maxPerRow);
   }
+
+  private chunkArray(myArray, chunk_size) {
+    var index = 0;
+    var arrayLength = myArray.length;
+    var tempArray = [];
+
+    for (index = 0; index < arrayLength; index += chunk_size) {
+      var myChunk = myArray.slice(index, index + chunk_size);
+      tempArray.push(myChunk);
+    }
+
+    return tempArray;
+  }
+
+  imageLoaded(event: any, image: any) {
+    console.log(event);
+    var htmlImageElement = event.path[0];
+    image.width = htmlImageElement.width;
+    image.height = htmlImageElement.height;
+    image.ratio = image.width / image.height;
+  }
+}
