64c62827ab0548ca26b0f56c152da6ad5f3fc191
xremix <tonidhoffmann@gmail.com>
2020-08-31 09:16:58 +0200

    first sample code for one row

--- projects/x-gallerify/src/lib/x-gallerify.component.ts
+++ projects/x-gallerify/src/lib/x-gallerify.component.ts
@@ -2,19 +2,52 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'ui-xGallerify',
-  template: `
-    <p>
-      x-gallerify works!
-    </p>
-  `,
-  styles: [
-  ]
+
+  templateUrl: './x-gallerify.component.html',
+  styleUrls: ['./x-gallerify.component.scss']
 })
 export class XGallerifyComponent implements OnInit {
-
+  public images: Array<any> = [
+      {
+        src: "https://farm66.staticflickr.com/65535/49489692872_a202029a13_b.jpg",
+      },
+      {
+        src: "https://farm66.staticflickr.com/65535/49489489251_21a1775a60_b.jpg",
+      },
+      {
+        src: "https://farm66.staticflickr.com/65535/49489694752_f8d8745a85_b.jpg",
+      },
+      {
+        src: "https://farm66.staticflickr.com/65535/49488995108_5283113839_b.jpg",
+      },
+      {
+        src: "https://farm66.staticflickr.com/65535/49488995108_5283113839_b.jpg",
+      },
+      {
+        src: "https://farm66.staticflickr.com/65535/49488995108_5283113839_b.jpg",
+      },
+      {
+        src: "https://farm66.staticflickr.com/65535/49488995108_5283113839_b.jpg",
+      }
+    ];
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+
+  imageLoaded(event: any){
+    console.log(event)
+    var img = event.path[0];
+    var width = img.width;
+    var height = img.height;
+    var ratio  = width / height;
+
+    for (let i = 0; i < this.images.length; i++) {
+      if(this.images[i].src === img.src){
+        this.images[i].ratio = ratio;
+      }
+    }
   }
 
 }
