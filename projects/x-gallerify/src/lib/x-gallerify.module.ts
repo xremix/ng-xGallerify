c8e3007203076a47977e6fe4c3dd9d71288fdd24
xremix <tonidhoffmann@gmail.com>
2020-09-01 22:43:34 +0200

    working on be able to publish the package

--- projects/x-gallerify/src/lib/x-gallerify.module.ts
+++ projects/x-gallerify/src/lib/x-gallerify.module.ts
@@ -2,13 +2,13 @@ import { NgModule } from '@angular/core';
 import { XGallerifyComponent } from './x-gallerify.component';
 import { GalleryImage } from './models/gallery-image';
 import { BreakPoint } from './models/breakpoint';
-import { GridService } from './services/grid';
+import { GridService } from './services/grid.service';
 
 
 @NgModule({
   declarations: [XGallerifyComponent],
   imports: [
   ],
-  exports: [XGallerifyComponent, GalleryImage, BreakPoint, GridService]
+  exports: [XGallerifyComponent]
 })
 export class XGallerifyModule { }
--- projects/x-gallerify/src/public-api.ts
+++ projects/x-gallerify/src/public-api.ts
@@ -4,3 +4,6 @@
 
 export * from './lib/x-gallerify.component';
 export * from './lib/x-gallerify.module';
+// export * from './lib/services/grid.service';
+// export * from './lib/models/breakpoint';
+// export * from './lib/models/gallery-image';
