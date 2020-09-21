8f816d2c3ed38eec6db2b729cecd8c066d0f13f4
xremix <tonidhoffmann@gmail.com>
2020-09-21 22:58:30 +0200

    upgrading version

--- projects/x-gallerify/src/lib/x-gallerify.module.ts
+++ projects/x-gallerify/src/lib/x-gallerify.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { XGallerifyComponent } from './x-gallerify.component';
-// import { GalleryImage } from './models/gallery-image';
 import { BreakPoint } from './models/breakpoint';
 import { GridService } from './services/grid.service';
 import { CommonModule } from '@angular/common';
